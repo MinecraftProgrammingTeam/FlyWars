3503a26c26c7381b0754e52946b3d5ee14937090
gingmzmzx <2417481092@qq.com>
2023-01-05 22:32:00 +0800

    改改改，改改改，改改改

--- src/main/java/top/mpt/xzystudio/flywars/game/Game.java
+++ src/main/java/top/mpt/xzystudio/flywars/game/Game.java
@@ -1,12 +1,11 @@
 package top.mpt.xzystudio.flywars.game;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.PlayerInventory;
+import top.mpt.xzystudio.flywars.Main;
+import top.mpt.xzystudio.flywars.scheduler.StartGame;
 import top.mpt.xzystudio.flywars.utils.ConfigUtils;
 import top.mpt.xzystudio.flywars.utils.ItemUtils;
 import top.mpt.xzystudio.flywars.utils.PlayerUtils;
@@ -78,31 +77,14 @@ public class Game {
      * 开始游戏
      */
     public void startGame() {
-        // 遍历team数组
-        for (Team team : teams) {
-            // 获取每个team的两名玩家
-            Player p1 = team.getP1();
-            Player p2 = team.getP2();
-            HashMap<Player, TeammateType> map = team.players;
-            // 遍历一个team的两名玩家的数组
-            for (Player p : Arrays.asList(p1, p2)) {
-                // 向玩家展示信息
-                PlayerUtils.send(p, "&f[FlyWars] &9你与 <%s> 组为一队", team.getTheOtherPlayer(p).getName());
-                PlayerUtils.showTitle(p, "&a游戏开始", "&cFlyWars 飞行战争");
-                p.setGameMode(GameMode.SURVIVAL);
-                if (map.get(p) == TeammateType.P1) {
-                    // 设置玩家飞行
-                    if (!p.getAllowFlight()) p.setAllowFlight(true);
-                    p.setFlying(true);
-                    // 清空玩家背包
-                    PlayerInventory inv = p.getInventory();
-                    inv.clear();
-                    // 给玩家发放物资
-                    inv.setChestplate(ItemUtils.newItem(Material.ELYTRA, ChatColor.AQUA + "战争鞘翅", new ArrayList<>(), 1, true));
-                    inv.setItemInOffHand(ItemUtils.newItem(Material.GOLDEN_APPLE, ChatColor.GOLD + "天赐金苹果"));
-                    inv.setItemInMainHand(ItemUtils.newItem(Material.FIREWORK_ROCKET, ChatColor.RED + "核弹"));
-                }
-            }
+        Player pl = (Player) sender;
+        for (Player p : Main.instance.getServer().getOnlinePlayers()){
+            Location loc = new Location(pl.getWorld(), Main.instance.getConfig().getInt("loc-x"), Main.instance.getConfig().getInt("loc-y"), Main.instance.getConfig().getInt("loc-z"));
+            p.teleport(loc);
+            p.sendTitle(ChatColor.RED + "游戏即将开始", "请保存好贵重物品，即将清空物品栏！");
         }
+
+        StartGame sg = new StartGame();
+        sg.runTaskLater(Main.instance, Main.instance.getConfig().getInt("delay-tick"));
     }
 }
