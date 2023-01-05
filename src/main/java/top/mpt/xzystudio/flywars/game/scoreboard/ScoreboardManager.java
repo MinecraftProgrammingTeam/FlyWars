3b7aa2bbc75660707807b431e87eb8f6b51158a8
gingmzmzx <2417481092@qq.com>
2023-01-05 18:31:14 +0800

    添加README

--- src/main/java/top/mpt/xzystudio/flywars/game/scoreboard/ScoreboardManager.java
+++ src/main/java/top/mpt/xzystudio/flywars/game/scoreboard/ScoreboardManager.java
@@ -1,5 +1,15 @@
 package top.mpt.xzystudio.flywars.game.scoreboard;
 
+import org.bukkit.scoreboard.Scoreboard;
+
 public class ScoreboardManager {
+    private final ArrayList<Scoreboard>
+    
     // https://bukkit.windit.net/javadoc/org/bukkit/scoreboard/Team.html
+    public ScoreboardManager() {
+    }
+    
+    public void initialScoreboards() {
+        new org.bukkit.scoreboard.ScoreboardManager()
+    }
 }
